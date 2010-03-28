5e565ea9afa417192abd4b4a26467a9b0b5da344
Peter Teichman <peter@teichman.org>
2010-03-28 09:27:54 -0400

    Remove bare string literals in daemon.py
    Found by pylint

--- kibot/daemon.py
+++ kibot/daemon.py
@@ -3,20 +3,19 @@ import sys
 import os
 import os.path
 
-'''This module is used to fork the current process into a daemon.
-    Almost none of this is necessary (or advisable) if your daemon
-    is being started by inetd. In that case, stdin, stdout and stderr are
-    all set up for you to refer to the network connection, and the fork()s
-    and session manipulation should not be done (to avoid confusing inetd).
-    Only the chdir() and umask() steps remain as useful.
-    References:
-        UNIX Programming FAQ
-            1.7 How do I get my program to act like a daemon?
-                http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+# This module is used to fork the current process into a daemon.
+# Almost none of this is necessary (or advisable) if your daemon
+# is being started by inetd. In that case, stdin, stdout and stderr are
+# all set up for you to refer to the network connection, and the fork()s
+# and session manipulation should not be done (to avoid confusing inetd).
+# Only the chdir() and umask() steps remain as useful.
+# References:
+#     UNIX Programming FAQ
+#         1.7 How do I get my program to act like a daemon?
+#             http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
 
-        Advanced Programming in the Unix Environment
-            W. Richard Stevens, 1992, Addison-Wesley, ISBN 0-201-56317-7.
-    '''
+#     Advanced Programming in the Unix Environment
+#         W. Richard Stevens, 1992, Addison-Wesley, ISBN 0-201-56317-7.
 
 def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
     '''This forks the current process into a daemon.
@@ -132,12 +131,12 @@ def test_lockfiles():
     if lock_file(filename):
         print 'created new lockfile [GOOD]'
     else:
-        'could not create lockfile [BAD]'
+        print 'could not create lockfile [BAD]'
 
     if lock_file(filename):
         print 'created SECOND lockfile [BAD]'
     else:
-        'could not create second lockfile [GOOD]'
+        print 'could not create second lockfile [GOOD]'
 
     if cleanup_old_lockfile(filename):
         print 'cleanup succeeded [BAD]'
